bfad94e7f7ad1ae35d9aa3267c2ce264b2e4c1c4
Thomas <thomasbruninx@users.noreply.github.com>
2022-02-15 22:49:40 +0100

    Initial commit

---
+++ debug.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+font = pygame.font.Font(None, 24)
+
+
+def debug(msg, x=2, y=2):
+    display_surface = pygame.display.get_surface()
+    debug_surf = font.render(str(msg), True, 'White')
+    debug_rect = debug_surf.get_rect(topleft = (x, y))
+    pygame.draw.rect(display_surface, 'Black', debug_rect)
+    display_surface.blit(debug_surf, debug_rect)
---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+from settings import *
+from level import Level
+from debug import debug
+
+
+class Game:
+    def __init__(self):
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
+        pygame.display.set_caption('RPG in need of a title')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
